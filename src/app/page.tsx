73c663d3be5eed3269899c12eb17cbdec1ca06e0
Lachezar Tsochev <ltsochev@live.com>
2025-01-17 13:36:39 +0200

    added todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,12 +32,17 @@ export default async function Home() {
             </div>
           )}
           {!user && (
-            <Link
-              href="/login"
-              className="h-12 px-4 py-2 w-full bg-purple-600 hover:bg-purple-700 mb-4 text-white rounded-md text-sm font-medium"
-            >
-              Go to login page
-            </Link>
+            <div className="flex gap-4 justify-center items-center">
+              <Link
+                href="/login"
+                className="px-4 py-2 w-full bg-purple-600 hover:bg-purple-700 text-white rounded-md text-sm font-medium"
+              >
+                Go to login page
+              </Link>
+              <span className="px-4 py-2 w-full bg-purple-600 hover:bg-purple-700 text-white rounded-md text-sm font-medium">
+                Request Upload URL
+              </span>
+            </div>
           )}
         </Hero>
         <Features />
